a78efb776663ec854bb9cc845bca9a12e97aaebf
k-m-e <k-m-e@yandex.ru>
2013-11-16 23:33:18 +0400

    Added ability to change namespace path in the config.

--- assets/components/sendex/js/mgr/sections/home.js
+++ assets/components/sendex/js/mgr/sections/home.js
@@ -1,7 +1,3 @@
-Ext.onReady(function() {
-	MODx.load({ xtype: 'sendex-page-home'});
-});
-
 Sendex.page.Home = function(config) {
 	config = config || {};
 	Ext.applyIf(config,{
@@ -9,7 +5,7 @@ Sendex.page.Home = function(config) {
 			xtype: 'sendex-panel-home'
 			,renderTo: 'sendex-panel-home-div'
 		}]
-	}); 
+	});
 	Sendex.page.Home.superclass.constructor.call(this,config);
 };
 Ext.extend(Sendex.page.Home,MODx.Component);
--- core/components/sendex/index.class.php
+++ core/components/sendex/index.class.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . '/model/sendex/sendex.class.php';
-
 /**
  * Class SendexMainController
  */
@@ -14,16 +12,19 @@ abstract class SendexMainController extends modExtraManagerController {
 	 * @return void
 	 */
 	public function initialize() {
+		$corePath = $this->modx->getOption('sendex_core_path', null, $this->modx->getOption('core_path') . 'components/sendex/');
+		require_once $corePath . 'model/sendex/sendex.class.php';
+
 		$this->Sendex = new Sendex($this->modx);
 
-		$this->modx->regClientCSS($this->Sendex->config['cssUrl'] . 'mgr/main.css');
-		$this->modx->regClientStartupScript($this->Sendex->config['jsUrl'] . 'mgr/sendex.js');
-		$this->modx->regClientStartupHTMLBlock('<script type="text/javascript">
-		Ext.onReady(function() {
-			Sendex.config = ' . $this->modx->toJSON($this->Sendex->config) . ';
-			Sendex.config.connector_url = "' . $this->Sendex->config['connectorUrl'] . '";
-		});
-		</script>');
+		$this->addCss($this->Sendex->config['cssUrl'] . 'mgr/main.css');
+		$this->addJavascript($this->Sendex->config['jsUrl'] . 'mgr/sendex.js');
+		$this->addHtml('<script type="text/javascript">
+                Ext.onReady(function() {
+                        Sendex.config = ' . $this->modx->toJSON($this->Sendex->config) . ';
+                        Sendex.config.connector_url = "' . $this->Sendex->config['connectorUrl'] . '";
+                });
+                </script>');
 
 		parent::initialize();
 	}
